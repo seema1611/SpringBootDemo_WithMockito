884983fe4d83ca0f2f857f578c22cbb91e5a58c4
seema1611 <seemarajpure16@gmail.com>
2020-06-25 19:59:45 +0530

    ADDED: Feature to add test case for save data

--- src/test/java/com/mockitodemo/MockitodemoApplicationTests.java
+++ src/test/java/com/mockitodemo/MockitodemoApplicationTests.java
@@ -1,16 +1,41 @@
 package com.mockitodemo;
 
+import com.mockitodemo.Service.implementors.UserService;
+import com.mockitodemo.model.User;
+import com.mockitodemo.repository.UserRepository;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class MockitodemoApplicationTests {
 
+    @Autowired
+    UserService userService;
+
+    @MockBean
+    UserRepository userRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void saveUserTest() {
+        User user = new User(3, "Amol", 30, "Pune");
+        when(userRepository.save(user)).thenReturn(user);
+        System.out.println(user.getId());
+        assertEquals(user, userService.addUser(user));
     }
 }
